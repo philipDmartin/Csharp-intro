21ae7f262186d15813496d53d0bb1a1246c51fdd
philipDmartin <philipnathan788@gmail.com>
2020-05-22 15:52:12 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1.
+            //refrencing other classes
+            var c1 = new MyClass();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+
+            //2.
+            //readline to hold users response
+            Console.Write("What is your first name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Where do you live?");
+            string address = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name} {lastName}! I'm glad to meet you. {address} is an awsome place to live!");
+        }
+    }
+}
---
+++ myClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Intro
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "Happy coding!";
+        }
+    }
+}
